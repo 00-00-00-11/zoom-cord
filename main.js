7f51dee7692dfd9cfd64c0c0b7763822d0a43c8d
sholt20 <samuelholt123@gmail.com>
2020-09-18 15:31:50 -0400

    Updated package and a minor readability tweak

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ client.on('message', message => {
      const args = message.content.slice(prefix.length).split(/ +/);
      const command = args.shift().toLowerCase();
 
-     if(command === 'ping') {
+     if (command === 'ping') {
           client.commands.get('ping').execute(message, args);
      } else if (command === 'angry') {
           client.commands.get('angry').execute(message, args);
